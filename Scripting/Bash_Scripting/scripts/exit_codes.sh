af2559c45d6521c6b8ccfe0601be9017c5dbd217
joesavio12 <joesavio11@gmail.com>
2024-12-26 16:19:40 +1100

    Exit Code Script

---
+++ Scripting/Bash_Scripting/scripts/exit_codes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+package=htop #uninstall htop to see the successful installation and exit code. try something random to see an unsuccessful installation and exit code
+
+sudo apt install $package
+
+if [ $? -eg 0 ]
+then
+	echo "The exit code for the package install is: $?"
+	echo "The new command is available here:"
+	which $package
+else
+	echo "$package failed to install."
+fi
