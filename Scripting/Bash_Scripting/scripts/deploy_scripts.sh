57a481d30cdade340926cd4d3ba51cb076b7712e
joesavio12 <joesavio11@gmail.com>
2025-01-04 18:54:17 +1100

    Deploy Script

---
+++ Scripting/Bash_Scripting/scripts/deploy_scripts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the source repository path and target directory
+REPO_PATH=/Workspace/linux-cloud-learning/Scripting/Bash_Scripting/scripts
+TARGET_DIR=$HOME
+
+# Copy the script to the home directory
+cp $REPO_PATH/script_name.sh" "$TARGET_DIR
+
+# Notify the user
+echo "Script has been copied to $HOME successfully!"
